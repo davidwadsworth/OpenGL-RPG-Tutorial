36f783e098347005e6cfabfd046f3d3c19a4d5c0
Young Gouda <34929021+davidwadsworth@users.noreply.github.com>
2021-04-05 16:47:02 -0500

    moving workstations
     - working on ep 12. TileMaps using RapidJson

---
+++ opengl_build_rpg_tutorial/Resources/Data/TestTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="TestTileset" tilewidth="32" tileheight="32" spacing="3" margin="1" tilecount="72" columns="23" objectalignment="topleft">
+ <image source="iq_shape_padding.png" width="804" height="104"/>
+</tileset>
---
+++ opengl_build_rpg_tutorial/delimiter_split.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <glad/glad.h>
+
+/*
+splits a string into x substrings dividing at all delimiter positions, returned as a vector
+
+eg. delimiter_split("jarvis.png", '.') -> {"jarvis", "png"}
+
+@author David Wadsworth
+*/
+
+std::vector<std::string> delimiter_split(const GLchar* str, GLchar delim)
+{
+	std::vector<std::string> result;
+	std::string temp;
+
+	while (*str)
+	{
+		// if delimiter is found pushback temp string into result list and start on the next string
+		if (*str == delim)
+		{
+			result.push_back(std::string(temp));
+			temp.clear();
+			str++;
+		}
+		temp.push_back(*str++);
+	}
+	result.push_back(std::string(temp));
+
+	return result;
+}
