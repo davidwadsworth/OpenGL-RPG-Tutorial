2be753c1bdf673e032a699f081bb422f5e552483
Young Gouda <34929021+davidwadsworth@users.noreply.github.com>
2021-04-09 15:51:37 -0500

    Trying out different texture packers
     - code and web paid is the smoothest
     - code and web free version seems to be the best free option
     - simplified some things to make life easier

--- opengl_build_rpg_tutorial/component_trigger_input_texture.h
+++ opengl_build_rpg_tutorial/component_trigger_input_texture.h
@@ -23,17 +23,9 @@ namespace Component {
 				void create(EntityMap& map) override final
 				{
 					auto flesh_tex_name = "resources/images/flesh_full.png";
-					auto tileset_tex_name = "resources/images/iq_shape_padding.png";
-					auto tileset_nsp_tex_name = "resources/images/iq_no_shape_padding.png";
 
 					auto& c_flesh_tex = *entity_->add_component_str_id<Component::Texture>("flesh_full");
 					c_flesh_tex.load(flesh_tex_name);
-
-					auto& c_tileset_tex = *entity_->add_component_str_id<Component::Texture>("iq_shape_padding");
-					c_tileset_tex.load(tileset_tex_name);
-
-					auto& c_tileset_nsp_tex = *entity_->add_component_str_id<Component::Texture>("iq_no_shape_padding");
-					c_tileset_tex.load(tileset_nsp_tex_name);
 				}
 			};
 		}
