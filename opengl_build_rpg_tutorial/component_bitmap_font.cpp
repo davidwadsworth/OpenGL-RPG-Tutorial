d3eed3542afbe1663c1634d2e9a887af86c7e43e
Young Gouda <34929021+davidwadsworth@users.noreply.github.com>
2021-06-15 23:16:48 -0500

    ep 15. Bitmap Fonts
     - trying to struggle through this, need more braincells.

--- opengl_build_rpg_tutorial/component_bitmap_font.cpp
+++ opengl_build_rpg_tutorial/component_bitmap_font.cpp
@@ -1,7 +1,7 @@
 #include "component_bitmap_font.h"
 #include "component_bitmap_glyph.h"
 
-Component::BitMapFont::MessageContainer Component::BitMapFont::generate_word(std::string word, std::vector<Component::Transform*> transforms, GLuint message_pos = 0u)
+Component::BitMapFont::MessageContainer Component::BitMapFont::generate_word(std::string word, Component::Transform& textbox, std::vector<Component::Transform*> transforms, GLuint& message_pos)
 {
     MessageContainer m_cont;
 
@@ -12,7 +12,10 @@ Component::BitMapFont::MessageContainer Component::BitMapFont::generate_word(std
 		auto font_char = font_->get_child(c);
 		
 		auto& c_bitmap_char = *font_char->get_component<Component::BitMapGlyph>();
+		auto& c_bitmap_src = *font_char->get_component<Component::Src>();
 
+		auto dest_x = textbox.x + c_bitmap_char.x_off + c_bitmap_char.check_kerning(c_bitmap_char, prev_char);
+		
 		m_cont.length += c_bitmap_char.advance + c_bitmap_char.check_kerning(prev_char);
 		prev_char = c;
 		m_cont.w.push_back(prev_char);
@@ -21,7 +24,7 @@ Component::BitMapFont::MessageContainer Component::BitMapFont::generate_word(std
     return m_cont;
 }
 
-Component::BitMapFont::MessageContainer Component::BitMapFont::generate_line(char* message, char delim, Component::Transform& textbox, float align_x, float pad_x, std::vector<Component::Transform*> transforms, GLuint message_pos)
+Component::BitMapFont::MessageContainer Component::BitMapFont::generate_line(char* message, char delim, Component::Transform& textbox, float align_x, float pad_x, std::vector<Component::Transform*> transforms, GLuint& message_pos)
 {
 	MessageContainer m_cont;
 
@@ -30,7 +33,7 @@ Component::BitMapFont::MessageContainer Component::BitMapFont::generate_line(cha
 	return MessageContainer();
 }
 
-Component::BitMapFont::MessageContainer Component::BitMapFont::generate_message(std::string message, Component::Transform& textbox, glm::vec2 align, glm::vec2 pad_x, std::vector<Component::Transform*> transforms, GLuint message_pos)
+Component::BitMapFont::MessageContainer Component::BitMapFont::generate_message(std::string message, Component::Transform& textbox, glm::vec2 align, glm::vec2 pad_x, std::vector<Component::Transform*> transforms, GLuint& message_pos)
 {
 	return MessageContainer();
 }
