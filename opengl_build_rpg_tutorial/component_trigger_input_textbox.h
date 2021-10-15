943b7ed49aaeb48c9a29fec7ff9c3765b35742ea
Young Gouda <34929021+davidwadsworth@users.noreply.github.com>
2021-10-15 05:51:11 -0500

    we are close

--- opengl_build_rpg_tutorial/component_trigger_input_textbox.h
+++ opengl_build_rpg_tutorial/component_trigger_input_textbox.h
@@ -6,6 +6,9 @@
 #include "json.hpp"
 #include "component_template.h"
 #include "component_tree.h"
+#include "component_material.h"
+#include "component_system_render_camera_draw.h"
+#include "component_controller_keyboard.h"
 
 /*
 @author David Wadsworth
@@ -59,7 +62,9 @@ namespace Component {
 					std::string align_h = msg_json["align_horizontal"];
 					std::string align_v = msg_json["align_vertical"];
 					float line_spacing = msg_json["line_spacing"];
-					float padding = msg_json["padding"];
+					float text_padding = msg_json["text_padding"];
+					float msg_padding_x = msg_json["msg_padding_x"];
+					float msg_padding_y = msg_json["msg_padding_y"];
 					int corner_size = msg_json["corner_size"];
 					bool speech_box = msg_json["speech_box"] == "true";
 					std::string message = msg_json["message"];
@@ -67,55 +72,106 @@ namespace Component {
 					// get font
 					auto font = gamestate->get_child("gilsans");
 
+					// get renderer
+					auto& c_renderer = *gamestate->get_child("renderer")->get_component<Component::Renderer>();
+
+					// get controller 
+					auto& c_cont_keyboard = *gamestate->get_child("controller")->get_component<Component::Controller::Keyboard>();
+
+					// get camera
+					auto& c_cam_transform = *gamestate->get_child("camera")->get_component<Component::Transform>();
+
+					// get font material
+					auto& c_font_material = *font->get_component<Component::Material>(0);
+
+					// get textbox shader and texture
+					auto& c_textbox_tex = *gamestate->get_child("texture manager")->get_component<Component::Texture>("flesh_full");
+					auto& c_sprite_shader = *gamestate->get_child("shader manager")->get_component<Component::Shader>("sprite");
+
+					// add textbox material
+					auto& c_tb_material = *entity_->add_component<Component::Material>(c_textbox_tex, c_sprite_shader, 4);
+
 					// create boxes
 
 					// HelpersBox::generate
 
 					auto line_h = font->get_component<Component::Integer>("line_h")->value;
 
-					auto box_x = pos_.x;
-					auto box_y = pos_.y;
+					auto box_x = pos_.x - msg_padding_x;
+					auto box_y = pos_.y - msg_padding_y - box_h;
 
 					line_h = line_h - static_cast<int>(line_h / 3.0f / line_spacing);
 
 					corner_size = corner_size * box_sc;
-					padding = padding + corner_size;
+					text_padding = text_padding + corner_size;
 
 					// event tree for textbox
 					auto& c_system_tree = *entity_->push_back_component<Component::TriggerTree>();
 
 					// box corners transforms
-					auto& c_top_left_corner_dest = *entity_->push_back_component<Component::Transform>(
+					auto& c_tl_corner_trans = *entity_->push_back_component<Component::Transform>(
 						Rect{ box_x, box_y, corner_size, corner_size }, box_sc);
-					auto& c_top_right_corner_dest = *entity_->push_back_component<Component::Transform>(
+					auto& c_tr_corner_trans = *entity_->push_back_component<Component::Transform>(
 						Rect{ box_x + box_w - corner_size, box_y, corner_size, corner_size }, box_sc);
-					auto& c_bot_left_corner_dest = *entity_->push_back_component<Component::Transform>(
+					auto& c_bl_corner_trans = *entity_->push_back_component<Component::Transform>(
 						Rect{ box_x, box_y + box_h - corner_size, corner_size, corner_size }, box_sc);
-					auto& c_bot_right_corner_dest = *entity_->push_back_component<Component::Transform>(
+					auto& c_br_corner_trans = *entity_->push_back_component<Component::Transform>(
 						Rect{ box_x + box_w - corner_size, box_y + box_h - corner_size, corner_size, corner_size }, box_sc);
 
 					// box side transforms
-					auto& c_top_side_dest = *entity_->push_back_component<Component::Transform>(
+					auto& c_t_side_trans = *entity_->push_back_component<Component::Transform>(
 						Rect{ box_x + corner_size, box_y, box_w - corner_size * 2, corner_size }, box_sc);
-					auto& c_left_side_dest = *entity_->push_back_component<Component::Transform>(
+					auto& c_l_side_trans = *entity_->push_back_component<Component::Transform>(
 						Rect{ box_x, box_y + corner_size, corner_size, box_h - corner_size * 2 }, box_sc);
-					auto& c_bot_side_dest = *entity_->push_back_component<Component::Transform>(
+					auto& c_b_side_trans = *entity_->push_back_component<Component::Transform>(
 						Rect{ box_x + corner_size, box_y + box_h - corner_size, box_w - corner_size * 2, corner_size }, box_sc);
-					auto& c_right_side_dest = *entity_->push_back_component<Component::Transform>(
+					auto& c_r_side_trans = *entity_->push_back_component<Component::Transform>(
 						Rect{ box_x + box_w - corner_size, box_y + corner_size, corner_size, box_h - corner_size * 2 }, box_sc);
 
 					// center transform
-
-					auto& c_center_dest = *entity_->push_back_component<Component::Transform>(
+					auto& c_center_trans = *entity_->push_back_component<Component::Transform>(
 						Rect{ box_x + corner_size, box_y + corner_size, box_w - corner_size * 2, box_h - corner_size * 2 }, box_sc);
 
+					// once more apropriate sprite sheets come into play these will be moved out into a different entity
+					auto& c_tl_corner_src = *entity_->push_back_component<Component::Src>(
+						Rect{ 0.0f, 0.0f, 10.0f, 10.0f });
+					auto& c_t_side_src = *entity_->push_back_component<Component::Src>(
+						Rect{ 10.0f, 0.0f, 10.0f, 10.0f });
+					auto& c_tr_corner_src = *entity_->push_back_component<Component::Src>(
+						Rect{ 20.0f, 0.0f, 10.0f, 10.0f });
+					auto& c_l_side_src = *entity_->push_back_component<Component::Src>(
+						Rect{ 0.0f, 10.0f, 10.0f, 10.0f });
+					auto& c_center_src = *entity_->push_back_component<Component::Src>(
+						Rect{ 10.0f, 0.0f, 10.0f, 10.0f });
+					auto& c_r_side_src = *entity_->push_back_component<Component::Src>(
+						Rect{ 20.0f, 10.0f, 10.0f, 10.0f });
+					auto& c_bl_corner_src = *entity_->push_back_component<Component::Src>(
+						Rect{ 0.0f, 20.0f, 10.0f, 10.0f });
+					auto& c_b_side_src = *entity_->push_back_component<Component::Src>(
+						Rect{ 10.0f, 20.0f, 10.0f, 10.0f });
+					auto& c_br_corner_src = *entity_->push_back_component<Component::Src>(
+						Rect{ 20.0f, 20.0f, 10.0f, 10.0f });
+
+
+                    auto csr_tl_corner_camera_draw = entity_->add_component<Component::System::Render::CameraDraw>(c_renderer, c_tl_corner_src, c_tl_corner_trans, c_tb_material, c_cam_transform);
+					auto csr_tr_corner_camera_draw = entity_->add_component<Component::System::Render::CameraDraw>(c_renderer, c_tr_corner_src, c_tr_corner_trans, c_tb_material, c_cam_transform);
+					auto csr_bl_corner_camera_draw = entity_->add_component<Component::System::Render::CameraDraw>(c_renderer, c_bl_corner_src, c_bl_corner_trans, c_tb_material, c_cam_transform);
+					auto csr_br_corner_camera_draw = entity_->add_component<Component::System::Render::CameraDraw>(c_renderer, c_br_corner_src, c_br_corner_trans, c_tb_material, c_cam_transform);
+					auto csr_t_side_camera_draw = entity_->add_component<Component::System::Render::CameraDraw>(c_renderer, c_t_side_src, c_t_side_trans, c_tb_material, c_cam_transform);
+					auto csr_l_side_camera_draw = entity_->add_component<Component::System::Render::CameraDraw>(c_renderer, c_l_side_src, c_l_side_trans, c_tb_material, c_cam_transform);
+					auto csr_r_side_camera_draw = entity_->add_component<Component::System::Render::CameraDraw>(c_renderer, c_r_side_src, c_r_side_trans, c_tb_material, c_cam_transform);
+					auto csr_b_side_camera_draw = entity_->add_component<Component::System::Render::CameraDraw>(c_renderer, c_b_side_src, c_b_side_trans, c_tb_material, c_cam_transform);
 
 					// arrow transform (used only with speech textboxes)
-					if (speech_box) auto lame = true;
-					//auto& c_arrow_dest = *entity_->push_back_component<
+					if (speech_box)
+					{
+						auto& c_arrow_dest = *entity_->push_back_component<Component::Transform>();
+						// TODO need to copy and paste old code from wayyyyy back repo project
+					}
 
 
-					auto current_pos = pos_ + padding;
+
+					auto current_pos = pos_ + text_padding;
 					auto curr_char = message.begin();
 					
 					Entity* current_tb = nullptr;
