765dd35855a7306339c562e384299471dd8a104f
YoungGouda <34929021+davidwadsworth@users.noreply.github.com>
2022-02-22 18:58:38 -0600

    load textbox fleshing

--- opengl_build_rpg_tutorial/component_trigger_load_textbox.h
+++ opengl_build_rpg_tutorial/component_trigger_load_textbox.h
@@ -15,7 +15,6 @@ box json
 	"corner_size" : float,
 	"textbox": [string, int],
 	"speech_arrow" : one of "right", "left", "none"
-	"position" : [float, float],
 }
 
 textarea json 
@@ -67,15 +66,59 @@ namespace Component {
 				
 					auto& c_cam_transform = *gamestate->get_child("camera")->get_component<Component::Transform>();
 
+					// set up load data for moving box on screen and removing after done.
 					std::stringstream ss_box_json;
 				
-					auto box_x = pos_x - c_cam_transform.x + box_w >= Game::width ? pos_x - c_cam_transform.x - box_w : pos_x - c_cam_transform.x;
+					auto speech_arrow_alignment = pos_x - c_cam_transform.x + box_w >= Game::width;
+					auto box_x = speech_arrow_alignment ? pos_x - c_cam_transform.x - box_w : pos_x - c_cam_transform.x;
 					auto box_y = pos_y - c_cam_transform.y - box_h - 2 * corner_size * box_sc;
 
-					ss_box_json << "{\"box_rect\":{" << box_x << "," << box_y << "," << box_h << "," << box_w << "},";
-					ss_box_json << "\"box_scale\":\"" << box_sc << "\",";
-					ss_box_json << "\"corner_size\":\""
+					auto speech_arrow_str = speech_box ? (speech_arrow_alignment ? "right" : "left") : "none";
 
+					ss_box_json << "{\"box_rect\":[\"" << box_x << "\",\"" << box_y << "\",\"" << box_h << "\",\"" << box_w << "\"],";
+					ss_box_json << "\"box_scale\":\"" << box_sc << "\",";
+					ss_box_json << "\"corner_size\":\"" << corner_size << "\",";
+					ss_box_json << "\"textbox\":[" << textbox_name << ",\"" << textbox_pos << "\",";
+					ss_box_json << "\"speech_arrow\":\"" << speech_arrow_str << "\",";
+
+					auto e_textbox = gamestate->get_child(textbox_name)->get_child(textbox_pos);
+					auto e_box = e_textbox->get_child("box");
+
+					auto box_json = nlohmann::json::parse(ss_box_json);
+					e_box->get_child("game_info")->get_component<Component::Trigger::Load::Box>("load_box_0")->load(box_json);
+
+					std::string offscreen_rect = "[\"" + std::to_string(Game::removed.x) + "\",\"" + std::to_string(Game::removed.y) + "\",\"0\",\"0\"],";
+					box_json["box_rect"] = nlohmann::json::parse(offscreen_rect);
+					
+					e_box->get_child("game_info")->get_component<Component::Trigger::Load::Box>("load_box_1")->load(box_json);
+
+					// set up load data for textarea
+					std::string parser = workflow["parser"];
+
+					if (parser == "message")
+					{
+						std::vector<std::string> messages = workflow["data"];
+
+						for (auto i = 0; i < messages.size(); ++i)
+						{
+							std::stringstream ss_txta_json;
+							ss_txta_json << "{\"textarea_rect\":[\"" << box_x + corner_size * box_sc / 2.0f + text_padding << "\",\"" << box_y + corner_size * box_sc / 2.0f + text_padding << "\",\"" 
+								<< box_h - corner_size * box_sc - 2.0f * text_padding << "\",\"" << box_w - corner_size * box_sc - 2.0f * text_padding << "\"],";
+							ss_txta_json << "\"font\":\"" << font_name << "\",";
+							ss_txta_json << "\"font_scale\":\"" << font_sc << "\",";
+							ss_txta_json << "\"align_horizontal\":\"" << align_h << "\",";
+							ss_txta_json << "\"align_vertical\":\"" << align_v << "\",";
+							ss_txta_json << "\"message\":\"" << messages[i] << "\",";
+							ss_txta_json << "\"textbox\":[\"" << textbox_name << "\",\"" << textbox_pos << "\"],";
+
+							auto msg_json = nlohmann::json::parse(ss_txta_json);
+
+						}
+					}
+					else if (parser == "option_box")
+					{
+						//TODO
+					}
 				}
 			};
 		} 
