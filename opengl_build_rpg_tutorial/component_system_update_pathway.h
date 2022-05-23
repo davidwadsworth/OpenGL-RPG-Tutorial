7d63c7176039ff0033b964c82f5667320e1c0545
Young Gouda <34929021+davidwadsworth@users.noreply.github.com>
2022-05-23 15:29:00 -0500

    still not there

--- opengl_build_rpg_tutorial/component_system_update_pathway.h
+++ opengl_build_rpg_tutorial/component_system_update_pathway.h
@@ -8,10 +8,36 @@ namespace Component {
 	namespace System {
 		namespace Update
 		{
-			class Pathway : public Component::ISystem, Tre<Component::ITrigger> 
+			class Pathway : public Component::ISystem
 			{
 				Component::Navigator& nav_;
+				std::vector<std::unique_ptr<Tree<std::vector<Component::ITrigger*>>>> created_trees_;
+				Tree<std::vector<Component::ITrigger*>>* curr_tree_;
 			public:
+				Pathway(Component::Navigator& nav)
+					: curr_tree_(nullptr), nav_(nav)
+				{}
+
+
+				Tree<std::vector<Component::ITrigger*>>* add_path(std::vector<ITrigger*> triggers)
+				{
+					auto n_tree = new Tree<std::vector<Component::ITrigger*>>{ {},  triggers };
+					created_trees_.push_back(std::unique_ptr<Tree<std::vector<Component::ITrigger*>>>(std::move(n_tree)));
+
+					curr_tree_ = n_tree;
+					return n_tree;
+				}
+
+				void clear_trees()
+				{
+					created_trees_.clear();
+				}
+
+				void set_curr(Tree<std::vector<Component::ITrigger*>>* tree)
+				{
+					curr_tree_ = tree;
+				}
+
 				void execute() override
 				{
 
