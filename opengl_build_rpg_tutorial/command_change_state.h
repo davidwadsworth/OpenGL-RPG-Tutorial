7d0ae82e5539386e0abbc28367871924f9feb8b7
YoungGouda <34929021+davidwadsworth@users.noreply.github.com>
2023-08-04 19:01:07 +1200

    change state update

--- opengl_build_rpg_tutorial/command_change_state.h
+++ opengl_build_rpg_tutorial/command_change_state.h
@@ -2,28 +2,35 @@
 #include "command.h"
 #include "game.h"
 #include "component_state.h"
+#include "component_engine.h"
 
 namespace Command
 {
 	class ChangeState : public ICommand
 	{
 		nlohmann::json new_state;
+		
+		nlohmann::json json_to_system(Entity* e_gamestate, nlohmann::json json)
+		{
+			
+		}
+
+	
 	public:
 
 		virtual void load(nlohmann::json json) 
 		{
 			new_state = json["load"];
 		}
+
+
 		virtual void execute(Entity* e_gamestate)
 		{
 			auto& c_state = *Game::global->get_component<Component::State>("state");
 			
-			for (auto r : new_state["render"])
-				if (r.get<std::string>() == "current")
-
-
-			
+			auto current = json_to_system(e_gamestate, new_state);
 			c_state.previous_state = c_state.current_state;
+			c_state.current_state = current;
 		}
 	};
 }
